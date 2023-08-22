9b1bef84d715367c4fdb0f46809f348cb3bb3667
Matheussop <matheussopluiz@outlook.com>
2023-08-22 19:22:12 -0300

    TodoList Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
+import styles from './App.module.css'
 import { Header } from './components/Header'
+
 
 function App() {
 
   return (
     <>
       <Header/>
-      <h1>Just a body</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.inputSimulation}>
+          <div>Just a input</div>
+        </div>
+
+        <div className={styles.todoList}>
+          <div className={styles.listHeader}>
+            <div className={styles.todoCreated}>
+              <text>
+                Tarefas criadas
+              </text>
+              <span>
+                0
+              </span>
+            </div>
+            <div className={styles.todoDone}>
+              <text>
+                Concluídas
+              </text>
+              <span>
+                0
+              </span>
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div>Just a body</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
