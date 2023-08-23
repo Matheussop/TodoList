a1600d4d4e25e7dc9a58a34c1ffe1f88016ca8c2
Matheussop <matheussopluiz@outlook.com>
2023-08-23 11:02:58 -0300

    feat: Components hover styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -13,26 +13,43 @@ interface TodoItemProps {
 
 export function TodoItem({textItem, onClickTrash, onSelected}: TodoItemProps) { 
   const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleSelectableItem = () => {
     setIsSelected((prevState) =>  !prevState )
     onSelected(!isSelected)
   }
+  
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
     <div className={styles.wrapper}>
       <div>
-        <button onClick={handleSelectableItem}>
+        <button onClick={handleSelectableItem}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           { isSelected ?
-            (<CheckCircle size="1.5rem" weight="fill" color="#5E60CE"/>)
+            (<CheckCircle className={styles.selectable} size="1.5rem"
+              weight="fill" 
+              color="#5E60CE"/>)
             :
-            (<Circle size="1.5rem" color="#4EA8DE"/>)
+            ( <Circle className={styles.notSelectable} 
+                weight={isHovered ? 'light' : 'duotone'} 
+                size="1.5rem" color="#4EA8DE">
+              </Circle>)
           }
         </button>
         <p className={isSelected ? styles.selectable : ''} >{textItem}</p>
       </div>
 
-      <button onClick={onClickTrash}>
+      <button onClick={onClickTrash} className={styles.btnTrash}>
         <Trash size="1.5rem"/>
       </button>
     </div>
